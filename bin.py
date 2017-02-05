3bb8b7184ea34c08024db2d27e1a693c27e15f5c
chybby <achybby@gmail.com>
2017-02-05 22:55:12 +1100

    totally renamed the folder...

--- bin.py
+++ bin.py
@@ -5,6 +5,7 @@ from __future__ import (
 
 import json
 import requests
+from time import sleep
 
 from flask import (
     Flask,
@@ -27,6 +28,13 @@ app.config.from_object('config')
 PAYLOAD_DONE = 'DONE'
 PAYLOAD_PASS = 'PASS'
 PAYLOAD_REMIND = 'REMIND'
+PAYLOAD_FORGOT = 'FORGOT'
+
+REDIS_ROSTER = 'roster'
+REDIS_PASSERS = 'passers'
+REDIS_BINS_DONE = 'bins_done'
+REDIS_REMIND = 'remind'
+REDIS_LAST_MESSAGED = 'last_messaged'
 
 
 def get_redis():
@@ -36,22 +44,166 @@ def get_redis():
     return redis
 
 
+def add_to_roster(user_id):
+    was_added = False
+    def _add_to_roster(pipe):
+        roster = pipe.lrange(REDIS_ROSTER, 0, -1)
+        if user_id not in roster:
+            roster_head = pipe.lpop(REDIS_ROSTER)
+            pipe.multi()
+            pipe.lpush(REDIS_ROSTER, user_id)
+            pipe.lpush(REDIS_ROSTER, roster_head)
+            was_added = True
+
+    get_redis().transaction(_add_to_roster, REDIS_ROSTER)
+
+    return was_added
+
+
+def get_last_messaged():
+    return get_redis().get(REDIS_LAST_MESSAGED)
+
+
+def set_remind():
+    with get_redis().pipeline() as pipe:
+        pipe.set(REDIS_REMIND, True)
+        pipe.set(REDIS_LAST_MESSAGED, None)
+
+def send_reminder():
+    needs_reminding = False
+    user_id = None
+    def _send_reminder(pipe):
+        needs_reminding = bool(pipe.get(REDIS_REMIND))
+        if needs_reminding:
+            user_id = pipe.lindex(REDIS_ROSTER, 0)
+            pipe.multi()
+            pipe.set(REDIS_LAST_MESSAGED, user_id)
+            pipe.set(REDIS_REMIND, False)
+
+    get_redis().transaction(_send_reminder, REDIS_REMIND, REDIS_ROSTER)
+    if needs_reminding:
+        send_bin_notification(user_id)
+
+def send_notification():
+    was_naughty = False
+    user_id = None
+    def _send_notification(pipe):
+        user_id = pipe.lindex(REDIS_ROSTER, 0)
+        bins_done = pipe.get(REDIS_BINS_DONE)
+        if not bins_done:
+            was_naughty = True
+        pipe.multi()
+        pipe.set(REDIS_LAST_MESSAGED, user_id)
+        pipe.set(REDIS_BINS_DONE, False)
+
+    get_redis().transaction(_send_notification, REDIS_ROSTER, REDIS_BINS_DONE)
+    if was_naughty:
+        send_naughty_notification(user_id)
+    else:
+        send_bin_notification(user_id)
+
+def mark_as_done():
+    def _mark_as_done(pipe):
+        done_id = pipe.lpop(REDIS_ROSTER)
+        pipe.multi()
+        pipe.rpush(REDIS_ROSTER, done_id)
+        pipe.set(REDIS_BINS_DONE, True)
+        pipe.ltrim(REDIS_PASSERS, 1, 0)
+        pipe.set(REDIS_LAST_MESSAGED, None)
+
+    get_redis().transaction(_mark_as_done, REDIS_ROSTER)
+
+def mark_as_forgot_done():
+    next_id = None
+
+    def _mark_as_forgot_done(pipe):
+        done_id = pipe.lpop(REDIS_ROSTER)
+        next_id = pipe.lindex(REDIS_ROSTER, 0)
+        pipe.multi()
+        pipe.rpush(REDIS_ROSTER, done_id)
+        pipe.set(REDIS_LAST_MESSAGED, next_id)
+        pipe.ltrim(REDIS_PASSERS, 1, 0)
+
+    get_redis().transaction(_mark_as_forgot_done, REDIS_ROSTER)
+    return next_id
+
+
+def mark_as_passed():
+    was_passed = True
+    next_id = None
+    passers = []
+    def _mark_as_passed(pipe):
+        passer_id = pipe.lpop(REDIS_ROSTER)
+        passers = pipe.lrange(REDIS_PASSERS, 0, -1)
+        if pipe.llen(REDIS_ROSTER) == len(passers):
+            # Everyone has passed
+            pipe.multi()
+            passers.append(passer_id)
+            for i in xrange(0, len(passers), -1):
+                pipe.lpush(REDIS_ROSTER, passers[i])
+            pipe.ltrim(REDIS_PASSERS, 1, 0)
+            was_passed = False
+        else:
+            for i in xrange(len(passers) + 1):
+                next_id = pipe.lpop(REDIS_ROSTER)
+            pipe.multi()
+            pipe.rpush(REDIS_PASSERS, passer_id)
+            passers.append(passer_id)
+            for i in xrange(0, len(passers), -1):
+                pipe.lpush(REDIS_ROSTER, passers[i])
+            pipe.lpush(REDIS_ROSTER, next_id)
+            pipe.set(REDIS_LAST_MESSAGED, next_id)
+
+    get_redis().transaction(_mark_as_passed, REDIS_ROSTER, REDIS_PASSERS)
+    return (was_passed, next_id, passers)
+
+
 def call_send_API(message_data):
     rs = requests.post('https://graph.facebook.com/v2.6/me/messages',
                        params={'access_token': PAGE_ACCESS_TOKEN},
                        json=message_data)
 
-    if rs.status_code == requests.codes.ok:
-        data = rs.json()
-        recipient_id = int(data['recipient_id'])
-        message_id = data['message_id']
-        print('Successfully sent message to %d with id %s' % (recipient_id,
-                                                              message_id))
-    else:
-        print('Unsuccessfully sent message (%d)', rs.status_code)
+    if rs.status_code != requests.codes.ok:
+        sleep(10)
+        call_send_API(message_data)
 
 
-def send_bin_notification(recipient_id):
+def send_naughty_notification(recipient_id):
+    message_data = {
+        'recipient': {
+            'id': recipient_id
+        },
+        'message': {
+            'text': 'You didn\'t empty me when I told you to! Do it today please',
+            'quick_replies': [
+                {
+                    'content_type': 'text',
+                    'title': 'Ok, done',
+                    'payload': PAYLOAD_DONE,
+                },
+                {
+                    'content_type': 'text',
+                    'title': 'I can\'t today',
+                    'payload': PAYLOAD_PASS,
+                },
+                {
+                    'content_type': 'text',
+                    'title': 'Remind me later',
+                    'payload': PAYLOAD_REMIND,
+                },
+                {
+                    'content_type': 'text',
+                    'title': 'I emptied you but forgot to tell you',
+                    'payload': PAYLOAD_FORGOT,
+                },
+            ]
+        }
+    }
+
+    call_send_API(message_data)
+
+
+def send_bin_notification(recipient_id, passers=[]):
     message_data = {
         'recipient': {
             'id': recipient_id
@@ -95,27 +247,47 @@ def send_text_message(recipient_id, text):
 
 
 def process_quick_reply(message):
-    sender_id = int(message['sender']['id'])
+    sender_id = message['sender']['id']
     time_of_message = int(message['timestamp'])
+
+    if sender_id != get_last_messaged():
+        # Someone is trying to send a quick reply when they shouldn't be able to
+        send_text_message(sender_id, "Stop trying to break me :(")
+        return
 
     payload = message['message']['quick_reply']['payload']
 
     if payload == PAYLOAD_DONE:
-        send_text_message(sender_id, 'Thanks buddy')
+        mark_as_done()
+        send_text_message(sender_id, 'Thanks buddy, I feel so refreshed')
     elif payload == PAYLOAD_PASS:
-        send_text_message(sender_id, 'Someone else will have to do it today')
+        was_passed, next_id, passers = mark_as_passed()
+        if not was_passed:
+            send_text_message(sender_id, 'Guess I\'m staying full today :(')
+        else:
+            send_text_message(sender_id, 'Ok, I\'ll ask someone else to empty me')
+            send_bin_notification(next_id, passers=passers)
     elif payload == PAYLOAD_REMIND:
+        # TODO: don't do this if it's past 8pm
+        set_remind()
         send_text_message(sender_id, 'Ok, I\'ll remind you this evening')
+    elif payload == PAYLOAD_FORGOT:
+        next_id = mark_as_forgot_done()
+        send_text_message(sender_id, 'All good, I\'ll ask someone else then')
+        send_bin_notification(next_id)
 
 
 def process_message(message):
-    sender_id = int(message['sender']['id'])
+    sender_id = message['sender']['id']
     time_of_message = int(message['timestamp'])
     message_text = message['message'].get('text')
 
-    send_text_message(sender_id, 'FYI: bot ain\'t ready yet')
-    send_bin_notification(sender_id)
+    send_naughty_notification(sender_id)
 
+    if add_to_roster(sender_id):
+        send_text_message(sender_id, 'The world could always use more bin emptiers')
+    else:
+        send_text_message(sender_id, 'Don\'t you have something better to do than talking to a bin?')
 
 @app.route('/')
 def index():
